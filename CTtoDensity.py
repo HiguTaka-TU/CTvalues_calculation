085094bc098d81e5ec0f42dc4a27ba9995345491
Takayuki Higuchi <takayuki@bayes.cc.h.u-tokyo.ac.jp>
2020-10-21 17:41:15 +0900

    CTvalues_calculation

--- CTtoDensity.py
+++ CTtoDensity.py
@@ -19,3 +19,25 @@ def CTtoDensityCurve_SB3_highest(csv_name):
 	filename='./CTtoDensity.png'
 	plt.savefig(filename)
 	plt.close()
+
+
+
+def CTtoDensity_NearestCurve(csv_name):
+	f1=np.loadtxt(csv_name,delimiter=',')
+
+	CT_values=np.array(f1)
+
+	B3F =[-1006.4,-726.6,-560.5,-84.2,-40.3,-1.6,-2.5,21,63.9,203.8,219.1,598.3,1058.6,1581.3]
+	QQ = [-1006.7,-704.3,-542.7,-94.4,-51.5,-13.3,-12.8,12.2,58.7,203,210.9,491.7,895.6,1338.3]
+	New =[-1006.7,-706.9,-533,-94.4,-44.5,-3.5,-3.7,18.4,68.9,218.7,230.1,428,795.1,1246.6]
+
+	B3F_nearest=1000000
+	QQ_nearest=1000000
+	New_nearest=1000000
+
+	dif_B3F=np.sum(np.square(CT_values-B3F),axis=1)
+	dif_QQ =np.sum(np.square(CT_values-QQ) ,axis=1)
+	dif_New=np.sum(np.square(CT_values-New),axis=1)
+
+	return np.argmin(dif_B3F),np.argmin(dif_QQ),np.argmin(dif_New)
+
