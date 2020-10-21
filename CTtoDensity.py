cc4a7e579af6948aa11bdff6de6a5b46b4e874a6
Takayuki Higuchi <takayuki@bayes.cc.h.u-tokyo.ac.jp>
2020-10-21 17:25:37 +0900

    CTvalues_calculation

--- CTtoDensity.py
+++ CTtoDensity.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-def CTtoDensityCurve_SB3_highest(csv1_name):
-	#csv1_name = './CTvalue_Noise0.0001.csv'
-	f1=np.loadtxt(csv1_name,delimiter=',')
+def CTtoDensityCurve_SB3_highest(csv_name):
+	f1=np.loadtxt(csv_name,delimiter=',')
 	CT_number=np.array(f1)
 
 	density=[0.002,0.28,0.40,0.942,0.977,1.0,1.018,1.053,1.097,1.143,1.154,1.335,1.56,1.825]
