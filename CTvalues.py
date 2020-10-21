eca079460ad01b47ec97f4bd3c1e544082f231dd
Takayuki Higuchi <takayuki@bayes.cc.h.u-tokyo.ac.jp>
2020-10-21 14:53:25 +0900

    CTvalues_calculation

--- CTvalues.py
+++ CTvalues.py
@@ -4,275 +4,155 @@ import matplotlib.pyplot as plt
 import csv
 
 
-width=512
-height=512
-
 def CTvalues():
 	PI=math.pi
 	r0=55
 	r00=105
 	r000=180
 
-	theta1=(45/2+45*2)*PI/180
-	x1=r00*math.cos(theta1)
-	y1=r00*math.sin(theta1)
-	theta2=(45/2+45*1)*PI/180
-	x2=r00*math.cos(theta2)
-	y2=r00*math.sin(theta2)
+	x=np.empty(17)
+	y=np.empty(17)
 
-	theta3=(45/2+45*0)*PI/180
-	x3=r00*math.cos(theta3)   
-	y3=r00*math.sin(theta3)
+	theta0=(45/2+45*2)*PI/180
+	x[0]=r00*math.cos(theta0)
+	y[0]=r00*math.sin(theta0)
 
-	theta4=(45/2+45*(-1))*PI/180
-	x4=r00*math.cos(theta4)
-	y4=r00*math.sin(theta4)
+	theta1=(45/2+45*1)*PI/180
+	x[1]=r00*math.cos(theta1)
+	y[1]=r00*math.sin(theta1)
 
-	theta5=(45/2+45*(-2))*PI/180
-	x5=r00*math.cos(theta5)
-	y5=r00*math.sin(theta5)
+	theta2=(45/2+45*0)*PI/180
+	x[2]=r00*math.cos(theta2)   
+	y[2]=r00*math.sin(theta2)
 
-	theta6=(45/2+45*(-3))*PI/180
-	x6=r00*math.cos(theta6)
-	y6=r00*math.sin(theta6)
+	theta3=(45/2+45*(-1))*PI/180
+	x[3]=r00*math.cos(theta3)
+	y[3]=r00*math.sin(theta3)
 
-	theta7=(45/2+45*(-4))*PI/180
-	x7=r00*math.cos(theta7)
-	y7=r00*math.sin(theta7)
+	theta4=(45/2+45*(-2))*PI/180
+	x[4]=r00*math.cos(theta4)
+	y[4]=r00*math.sin(theta4)
 
-	theta8=(45/2+45*(-5))*PI/180
-	x8=r00*math.cos(theta8)
-	y8=r00*math.sin(theta8)
+	theta5=(45/2+45*(-3))*PI/180
+	x[5]=r00*math.cos(theta5)
+	y[5]=r00*math.sin(theta5)
 
-	theta9=45*2*PI/180
-	x9=r0*math.cos(theta9)
-	y9=r0*math.sin(theta9)
+	theta6=(45/2+45*(-4))*PI/180
+	x[6]=r00*math.cos(theta6)
+	y[6]=r00*math.sin(theta6)
 
-	theta10=45*1*PI/180
-	x10=r0*math.cos(theta10)
-	y10=r0*math.sin(theta10)
+	theta7=(45/2+45*(-5))*PI/180
+	x[7]=r00*math.cos(theta7)
+	y[7]=r00*math.sin(theta7)
 
-	theta11=45*0*PI/180
-	x11=r0*math.cos(theta11)
-	y11=r0*math.sin(theta11)
+	theta8=45*2*PI/180
+	x[8]=r0*math.cos(theta8)
+	y[8]=r0*math.sin(theta8)
 
-	theta12=45*(-1)*PI/180
-	x12=r0*math.cos(theta12)
-	y12=r0*math.sin(theta12)
+	theta9=45*1*PI/180
+	x[9]=r0*math.cos(theta9)
+	y[9]=r0*math.sin(theta9)
 
-	theta13=45*(-2)*PI/180
-	x13=r0*math.cos(theta13)
-	y13=r0*math.sin(theta13)
+	theta10=45*0*PI/180
+	x[10]=r0*math.cos(theta10)
+	y[10]=r0*math.sin(theta10)
 
-	theta14=45*(-3)*PI/180
-	x14=r0*math.cos(theta14)
-	y14=r0*math.sin(theta14)
+	theta11=45*(-1)*PI/180
+	x[11]=r0*math.cos(theta11)
+	y[11]=r0*math.sin(theta11)
 
-	theta15=45*(-4)*PI/180
-	x15=r0*math.cos(theta15)
-	y15=r0*math.sin(theta15)
+	theta12=45*(-2)*PI/180
+	x[12]=r0*math.cos(theta12)
+	y[12]=r0*math.sin(theta12)
 
-	theta16=45*(-5)*PI/180
-	x16=r0*math.cos(theta16)
-	y16=r0*math.sin(theta16)
+	theta13=45*(-3)*PI/180
+	x[13]=r0*math.cos(theta13)
+	y[13]=r0*math.sin(theta13)
+
+	theta14=45*(-4)*PI/180
+	x[14]=r0*math.cos(theta14)
+	y[14]=r0*math.sin(theta14)
+
+	theta15=45*(-5)*PI/180
+	x[15]=r0*math.cos(theta15)
+	y[15]=r0*math.sin(theta15)
 
 	#Air Angle=0
-	xAir=r000*math.sin(theta11)
-	yAir=r000*math.cos(theta11)
+	x[16]=r000*math.sin(theta10)
+	y[16]=r000*math.cos(theta10)
 
-	#print(x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16)
-	#print(y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13,y14,y15,y16)
 
 	#sum of mu for average
-	sum1mu=0
-	sum2mu=0
-	sum3mu=0
-	sum4mu=0
-	sum5mu=0
-	sum6mu=0
-	sum7mu=0
-	sum8mu=0
-	sum9mu=0
-	sum10mu=0
-	sum11mu=0
-	sum12mu=0
-	sum13mu=0
-        sum14mu=0
-        sum15mu=0
-        sum16mu=0
-        sumAirmu=0
-
+	sum=np.zeros(17)
         #sum of pixel for average
-        sum1count=0
-        sum2count=0
-        sum3count=0
-        sum4count=0
-        sum5count=0
-        sum6count=0
-        sum7count=0
-        sum8count=0
-        sum9count=0
-        sum10count=0
-        sum11count=0
-        sum12count=0
-        sum13count=0
-        sum14count=0
-        sum15count=0
-        sum16count=0
-        sumAircount=0
-
-        #average of mu
-        ave1=0
-        ave2=0
-        ave3=0
-        ave4=0
-        ave5=0
-        ave6=0
-        ave7=0
-        ave8=0
-        ave9=0
-        ave10=0
-        ave11=0
-        ave12=0
-        ave13=0
-        ave14=0
-        ave15=0
-        ave16=0
-        aveAir=0
-
+	count=np.zeros(17)
+	
         for iy in range(height):
-                y =-iy+height/2-0.5
+                Y =-iy+height/2-0.5
                 for ix in range(width):
-                        x=ix-width/2+0.5
-                        d1=(x-x1)*(x-x1)+(y-y1)*(y-y1)
-                        d2=(x-x2)*(x-x2)+(y-y2)*(y-y2)
-                        d3=(x-x3)*(x-x3)+(y-y3)*(y-y3)
-                        d4=(x-x4)*(x-x4)+(y-y4)*(y-y4)
-                        d5=(x-x5)*(x-x5)+(y-y5)*(y-y5)
-                        d6=(x-x6)*(x-x6)+(y-y6)*(y-y6)
-                        d7=(x-x7)*(x-x7)+(y-y7)*(y-y7)
-                        d8=(x-x8)*(x-x8)+(y-y8)*(y-y8)
-                        d9=(x-x9)*(x-x9)+(y-y9)*(y-y9)
-                        d10=(x-x10)*(x-x10)+(y-y10)*(y-y10)
-                        d11=(x-x11)*(x-x11)+(y-y11)*(y-y11)
-                        d12=(x-x12)*(x-x12)+(y-y12)*(y-y12)
-                        d13=(x-x13)*(x-x13)+(y-y13)*(y-y13)
-                        d14=(x-x14)*(x-x14)+(y-y14)*(y-y14)
-                        d15=(x-x15)*(x-x15)+(y-y15)*(y-y15)
-                        d16=(x-x16)*(x-x16)+(y-y16)*(y-y16)
-                        dAir=(x-xAir)*(x-xAir)+(y-yAir)*(y-yAir)
-
-                        if d1<=25:
-                                #print(f[width*iy+ix])
-                                sum1mu+=f[ix+iy*width]
-                                sum1count+=1
-                        elif d2<=25:
-                                #print(f[width*iy+ix])
-                                sum2mu+=f[ix+iy*width]
-                                sum2count+=1
-                        elif d3<=25:
-                                #print(f[width*iy+ix])
-                                sum3mu+=f[ix+iy*width]
-                                sum3count+=1
-                        elif d4<=25:
-                                #print(f[width*iy+ix])
-                                sum4mu+=f[ix+iy*width]
-                                sum4count+=1
-                        elif d5<=25:
-                                #print(f[width*iy+ix])
-                                sum5mu+=f[ix+iy*width]
-                                sum5count+=1
-                        elif d6<=25:
-                                #print(f[width*iy+ix])
-                                sum6mu+=f[ix+iy*width]
-                                sum6count+=1
-                        elif d7<=25:
-                                #print(f[width*iy+ix])
-                                sum7mu+=f[ix+iy*width]
-                                sum7count+=1
-                        elif d8<=25:
-                                #print(f[width*iy+ix])
-                                sum8mu+=f[ix+iy*width]
-                                sum8count+=1
-                        elif d9<=25:
-                                #print(f[width*iy+ix])
-                                sum9mu+=f[ix+iy*width]
-                                sum9count+=1
-                        elif d10<=25:
-                                #print(f[width*iy+ix])
-                                sum10mu+=f[ix+iy*width]
-                                sum10count+=1
-                        elif d11<=25:
-                                #print(f[width*iy+ix])
-                                sum11mu+=f[ix+iy*width]
-                                sum11count+=1
-                        elif d12<=25:
-                                #print(f[width*iy+ix])
-                                sum12mu+=f[ix+iy*width]
-                                sum12count+=1
-                        elif d13<=25:
-                                #print(f[width*iy+ix])
-                                sum13mu+=f[ix+iy*width]
-                                sum13count+=1
-                        elif d14<=25:
-                                #print(f[width*iy+ix])
-                                sum14mu+=f[ix+iy*width]
-                                sum14count+=1
-                        elif d15<=25:
-                                #print(f[width*iy+ix])
-                                sum15mu+=f[ix+iy*width]
-                                sum15count+=1
-                        elif d16<=25:
-                                #print(f[width*iy+ix])
-                                sum16mu+=f[ix+iy*width]
-                                sum16count+=1
-                        elif dAir<=25:
-                                #print(f[width*iy+ix])
-                                sumAirmu+=f[ix+iy*width]
-                                sumAircount+=1
+                        X=ix-width/2+0.5
+                        d=(X-x)*(X-x)+(Y-y)*(Y-y)
+			if d[0]<=25:
+                                sum[0]+=f[ix+iy*width]
+                                count[0]+=1
+                        elif d[1]<=25:
+                                sum[1]+=f[ix+iy*width]
+                                count[1]+=1
+                        elif d[2]<=25:
+                                sum[2]+=f[ix+iy*width]
+                                count[2]+=1
+                        elif d[3]<=25:
+                                sum[3]+=f[ix+iy*width]
+                                count[3]+=1
+                        elif d[4]<=25:
+                                sum[4]+=f[ix+iy*width]
+                                count[4]+=1
+                        elif d[5]<=25:
+                                sum[5]+=f[ix+iy*width]
+                                count[5]+=1
+                        elif d[6]<=25:
+                                sum[6]+=f[ix+iy*width]
+                                count[6]+=1
+                        elif d[7]<=25:
+                                sum[7]+=f[ix+iy*width]
+                                count[7]+=1
+                        elif d[8]<=25:
+                                sum[8]+=f[ix+iy*width]
+                                count[8]+=1
+                        elif d[9]<=25:
+                                sum[9]+=f[ix+iy*width]
+                                count[9]+=1
+                        elif d[10]<=25:
+                                sum[10]+=f[ix+iy*width]
+                                count[10]+=1
+                        elif d[11]<=25:
+                                sum[11]+=f[ix+iy*width]
+                                count[11]+=1
+                        elif d[12]<=25:
+                                sum[12]+=f[ix+iy*width]
+                                count[12]+=1
+                        elif d[13]<=25:
+                                sum[13]+=f[ix+iy*width]
+                                count[13]+=1
+                        elif d[14]<=25:
+                                sum[14]+=f[ix+iy*width]
+                                count[14]+=1
+                        elif d[15]<=25:
+                                sum[15]+=f[ix+iy*width]
+                                count[15]+=1
+                        elif d[16]<=25:
+                                sum[16]+=f[ix+iy*width]
+                                count[16]+=1
                                                                                
-                                fd.close()
-	print(sum3count)
-	print(sum4count)
         #calculate average
-        ave1=sum1mu/sum1count
-        ave2=sum2mu/sum2count
-        ave3=sum3mu/sum3count
-        ave4=sum4mu/sum4count
-        ave5=sum5mu/sum5count
-        ave6=sum6mu/sum6count
-        ave7=sum7mu/sum7count
-        ave8=sum8mu/sum8count
-        ave9=sum9mu/sum9count
-        ave10=sum10mu/sum10count
-        ave11=sum11mu/sum11count
-        ave12=sum12mu/sum12count
-        ave13=sum13mu/sum13count
-        ave14=sum14mu/sum14count
-        ave15=sum15mu/sum15count
-        ave16=sum16mu/sum16count
-        aveAir=sumAirmu/sumAircount
+	ave=sum/count
+	
+	CT=(ave-ave[14])/ave[14]*1000
 
-	CT1=(ave1-ave15)/ave15*1000#Solid water
-	CT2=(ave2-ave15)/ave15*1000#LN300
-	CT3=(ave3-ave15)/ave15*1000#Solid water
-	CT4=(ave4-ave15)/ave15*1000#IB3
-        CT5=(ave5-ave15)/ave15*1000#Solid water
-        CT6=(ave6-ave15)/ave15*1000#LV1
-        CT7=(ave7-ave15)/ave15*1000#B200
-        CT8=(ave8-ave15)/ave15*1000#LN450
-        CT9=(ave9-ave15)/ave15*1000#SB3-Cortical
-        CT10=(ave10-ave15)/ave15*1000#SR2-Brain
-        CT11=(ave11-ave15)/ave15*1000#AP6
-        CT12=(ave12-ave15)/ave15*1000#CB2-50%
-        CT13=(ave13-ave15)/ave15*1000#Solid water
-        CT14=(ave14-ave15)/ave15*1000#CB2-30%
-        CT15=(ave15-ave15)/ave15*1000#Water
-        CT16=(ave16-ave15)/ave15*1000#BR12
-        CTAir=(aveAir-ave15)/ave15*1000#Air
+	return CT[16],CT[1],CT[7],CT[10],CT[15],CT[14],CT[0],CT[9],CT[5],CT[3],CT[6],CT[13],CT[11],CT[8]
 
-	return CTAir,CT2,CT8,CT11,CT16,CT15,CT1,CT10,CT6,CT4,CT7,CT14,CT12,CT9
-
+width=512
+height=512
 
 for i in range(1,2):
 #for i in range(1,2):
@@ -281,6 +161,7 @@ for i in range(1,2):
 	f=np.fromfile(fd,dtype=np.float32,count=height*width)
 	#img=f.reshape((height,width))
 	CTvalues=CTvalues()
+	fd.close()
 	with open('CTvalues_abs.csv','a') as f:
 		writer=csv.writer(f)
 		#writer.writerow([CTAir,CT2,CT8,CT11,CT16,CT15,CT1,CT10,CT6,CT4,CT7,CT14,CT12,CT9])
