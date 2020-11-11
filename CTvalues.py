6ff9fd389a5a080b1c7cf6daecde4196fb1e677b
HiguTaka <higuchitakayuki.tokushima@gmail.com>
2020-11-11 06:13:04 +0000

    use class

--- CTvalues.py
+++ CTvalues.py
@@ -1,166 +1,117 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 import csv
 
-
-def CTvalues():
+#GammexファントムのCT画像から各インサートのCT値を計算するクラス
+class CTvalues():
 	PI=math.pi
 	r0=55
 	r00=105
 	r000=180
-
-	x=np.empty(17)
-	y=np.empty(17)
-
-	theta0=(45/2+45*2)*PI/180
-	x[0]=r00*math.cos(theta0)
-	y[0]=r00*math.sin(theta0)
-
-	theta1=(45/2+45*1)*PI/180
-	x[1]=r00*math.cos(theta1)
-	y[1]=r00*math.sin(theta1)
-
-	theta2=(45/2+45*0)*PI/180
-	x[2]=r00*math.cos(theta2)   
-	y[2]=r00*math.sin(theta2)
-
-	theta3=(45/2+45*(-1))*PI/180
-	x[3]=r00*math.cos(theta3)
-	y[3]=r00*math.sin(theta3)
-
-	theta4=(45/2+45*(-2))*PI/180
-	x[4]=r00*math.cos(theta4)
-	y[4]=r00*math.sin(theta4)
-
-	theta5=(45/2+45*(-3))*PI/180
-	x[5]=r00*math.cos(theta5)
-	y[5]=r00*math.sin(theta5)
-
-	theta6=(45/2+45*(-4))*PI/180
-	x[6]=r00*math.cos(theta6)
-	y[6]=r00*math.sin(theta6)
-
-	theta7=(45/2+45*(-5))*PI/180
-	x[7]=r00*math.cos(theta7)
-	y[7]=r00*math.sin(theta7)
-
-	theta8=45*2*PI/180
-	x[8]=r0*math.cos(theta8)
-	y[8]=r0*math.sin(theta8)
-
-	theta9=45*1*PI/180
-	x[9]=r0*math.cos(theta9)
-	y[9]=r0*math.sin(theta9)
-
-	theta10=45*0*PI/180
-	x[10]=r0*math.cos(theta10)
-	y[10]=r0*math.sin(theta10)
-
-	theta11=45*(-1)*PI/180
-	x[11]=r0*math.cos(theta11)
-	y[11]=r0*math.sin(theta11)
-
-	theta12=45*(-2)*PI/180
-	x[12]=r0*math.cos(theta12)
-	y[12]=r0*math.sin(theta12)
-
-	theta13=45*(-3)*PI/180
-	x[13]=r0*math.cos(theta13)
-	y[13]=r0*math.sin(theta13)
-
-	theta14=45*(-4)*PI/180
-	x[14]=r0*math.cos(theta14)
-	y[14]=r0*math.sin(theta14)
-
-	theta15=45*(-5)*PI/180
-	x[15]=r0*math.cos(theta15)
-	y[15]=r0*math.sin(theta15)
-
-	#Air Angle=0
-	x[16]=r000*math.sin(theta10)
-	y[16]=r000*math.cos(theta10)
+		
+	def __init__(self,inner_insert_number,outer_insert_number):
+		self.inner_insert_number=inner_insert_number
+		self.outer_insert_number=outer_insert_number
+		self.theta=[]
+		self.x=[]
+		self.y=[]
+		self.distance=[0]*14
+		self.sum=[0]*14
+		self.count=[0]*14
+		self.average=[0]*14
+		self.ct=[]
 
 
-	#sum of mu for average
-	sum=np.zeros(17)
-        #sum of pixel for average
-	count=np.zeros(17)
+	#角度を計算
+	def calc_inner_theta(self,insert_number):
+		self.theta.append(45*insert_number*CTvalues.PI/180)
+
+	def calc_outer_theta(self,insert_number):
+		self.theta.append((45/2+45*insert_number)*CTvalues.PI/180)
 	
-        for iy in range(height):
-                Y =-iy+height/2-0.5
-                for ix in range(width):
-                        X=ix-width/2+0.5
-                        d=(X-x)*(X-x)+(Y-y)*(Y-y)
-			if d[0]<=25:
-                                sum[0]+=f[ix+iy*width]
-                                count[0]+=1
-                        elif d[1]<=25:
-                                sum[1]+=f[ix+iy*width]
-                                count[1]+=1
-                        elif d[2]<=25:
-                                sum[2]+=f[ix+iy*width]
-                                count[2]+=1
-                        elif d[3]<=25:
-                                sum[3]+=f[ix+iy*width]
-                                count[3]+=1
-                        elif d[4]<=25:
-                                sum[4]+=f[ix+iy*width]
-                                count[4]+=1
-                        elif d[5]<=25:
-                                sum[5]+=f[ix+iy*width]
-                                count[5]+=1
-                        elif d[6]<=25:
-                                sum[6]+=f[ix+iy*width]
-                                count[6]+=1
-                        elif d[7]<=25:
-                                sum[7]+=f[ix+iy*width]
-                                count[7]+=1
-                        elif d[8]<=25:
-                                sum[8]+=f[ix+iy*width]
-                                count[8]+=1
-                        elif d[9]<=25:
-                                sum[9]+=f[ix+iy*width]
-                                count[9]+=1
-                        elif d[10]<=25:
-                                sum[10]+=f[ix+iy*width]
-                                count[10]+=1
-                        elif d[11]<=25:
-                                sum[11]+=f[ix+iy*width]
-                                count[11]+=1
-                        elif d[12]<=25:
-                                sum[12]+=f[ix+iy*width]
-                                count[12]+=1
-                        elif d[13]<=25:
-                                sum[13]+=f[ix+iy*width]
-                                count[13]+=1
-                        elif d[14]<=25:
-                                sum[14]+=f[ix+iy*width]
-                                count[14]+=1
-                        elif d[15]<=25:
-                                sum[15]+=f[ix+iy*width]
-                                count[15]+=1
-                        elif d[16]<=25:
-                                sum[16]+=f[ix+iy*width]
-                                count[16]+=1
-                                                                               
-        #calculate average
-	ave=sum/count
+	#座標を計算
+	def calc_inner_coordinate(self,theta):
+		self.x.append(CTvalues.r0*math.cos(theta))
+		self.y.append(CTvalues.r0*math.sin(theta))
 	
-	CT=(ave-ave[14])/ave[14]*1000
+	def calc_outer_coordinate(self,theta):
+		self.x.append(CTvalues.r00*math.cos(theta))
+		self.y.append(CTvalues.r00*math.sin(theta))
 
-	return CT[16],CT[1],CT[7],CT[10],CT[15],CT[14],CT[0],CT[9],CT[5],CT[3],CT[6],CT[13],CT[11],CT[8]
+	def calc_air_coordinate(self):
+		self.x.append(CTvalues.r000*math.sin(0))
+		self.y.append(CTvalues.r000*math.cos(0))
+	
+	#該当するインサートのCT値の合計とカウントを計算
+	def calc_sum_and_count(self):	
+		for iy in range(height):
+			Y =-iy+height/2-0.5
+			for ix in range(width):
+				X=ix-width/2+0.5
+				for k in range(14):
+					self.distance[k]=(X-self.x[k])*(X-self.x[k])+(Y-self.y[k])*(Y-self.y[k])
+					if self.distance[k]<=25:
+						self.sum[k]+=f[ix+iy*width]
+						self.count[k]+=1
+	#CT値の計算        
+	def calc_ctvalues(self):   
+		for i in range(14):                                                                    
+			self.average[i]=self.sum[i]/self.count[i]
+		
+		for i in range(14):
+			self.ct.append((self.average[i]-self.average[4])/self.average[4]*1000)
 
-width=512
-height=512
+		return self.ct[13],self.ct[8],self.ct[9],self.ct[0],self.ct[3],self.ct[4],self.ct[7],self.ct[1],self.ct[11],self.ct[12],self.ct[10],self.ct[5],self.ct[6],self.ct[2]
 
-for i in range(1,2):
-	rawpath="/mnt/nfs_S65/Takayuki/package_TotalDensityEstimation/Recon/Recon7500/FBP_virtual_projection_512x512_gammex%d.raw" % i 
+def open_CTimages(rawpath):
 	fd=open(rawpath,'rb')
 	f=np.fromfile(fd,dtype=np.float32,count=height*width)
-	#img=f.reshape((height,width))
-	CTvalues=CTvalues()
-	fd.close()
+	return f,fd
+
+
+def main():	
+	#インサートの番号を入力
+	inner_insert_number=[0,1,2,3,4,5,7]
+	outer_insert_number=[2,1,3,4,5,7]
+	
+	#インスタンスを生成
+	ctvalues=CTvalues(inner_insert_number,outer_insert_number)
+	
+	#各インサートの角度を計算
+	for number in inner_insert_number:
+		ctvalues.calc_inner_theta(number)
+
+	for number in outer_insert_number:
+		ctvalues.calc_outer_theta(number)
+	
+	#各インサートの座標を計算
+	for number in range(len(inner_insert_number)):
+		ctvalues.calc_inner_coordinate(ctvalues.theta[number])
+	
+	for number in range(len(inner_insert_number),len(inner_insert_number)+len(outer_insert_number)):
+		ctvalues.calc_outer_coordinate(ctvalues.theta[number])
+	
+	ctvalues.calc_air_coordinate()
+
+	#各インサートの減弱係数の合計とカウントを計算
+	ctvalues.calc_sum_and_count()
+
+	#各インサートのCT値を計算
+	ctvalues13=ctvalues.calc_ctvalues()
+
+
 	with open('CTvalues.csv','a') as f:
 		writer=csv.writer(f)
-		writer.writerow(CTvalues)
+		writer.writerow(ctvalues13)
+
+
+
+if __name__=="__main__":
+	width=height=512
+	rawpath="/mnt/nfs_S65/Takayuki/package_TotalDensityEstimation/Recon/Recon7500/FBP_virtual_projection_512x512_gammex1.raw" 
+	#img=f.reshape((height,width))
+	f,fd=open_CTimages(rawpath)
+	main()
+	fd.close()
--- Evaluation.py
+++ Evaluation.py
@@ -1,6 +1,7 @@
 import csv 
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 def MAE_fig(csv_name):
 	f1=np.loadtxt(csv_name,delimiter=',')
@@ -32,19 +33,74 @@ def RMSE_fig(csv_name):
 	plt.savefig(filename)
 	plt.close()
 
-def SNR(f):
-        mu=[]
-        sum_mu=0
-        sum_count=0 
-        for iy in range(height):
-                y =-iy+height/2-0.5
-                for ix in range(width):
-                        x=ix-width/2+0.5
-                        d=x*x+y*y 
+def SNR(f,width,height):
+	mu=[]
+	SNR1_all=[]
+	SNR2_all=[]
+	sum_mu=0
+	sum_count=0 
+	for iy in range(height):
+		y =-iy+height/2-0.5
+		for ix in range(width):
+			x=ix-width/2+0.5
+			d=x*x+y*y 
     
-                        if d<=25:
-                                mu.append(f[ix+iy*width])
-        mean=np.mean(mu)
-        std=np.std(mu)
-        SNR=10*math.log10(mean/std)
+			if d<=25:
+				mu.append(f[ix+iy*width])
+	mean=np.mean(mu)
+	std=np.std(mu)
+	SNR1=10*math.log10(mean/std)
 	SNR2=mean/std
+
+	SNR1_all.append(SNR1)
+	SNR2_all.append(SNR2)
+
+	return SNR1_all,SNR2_all
+
+def Check_SNR_MeanEnergy():
+	SNR_file1='SNR7500.csv'
+	#SNR_file2='SNR_Noise0.0002.csv'
+	#SNR_file3='SNR_Noise0.0003.csv'
+	#SNR_file4='SNR_Noise0.0004.csv'
+	#SNR_file5='SNR_Noise0.0005.csv'
+	#SNR_file6='SNR_Noise0.0006.csv'
+	#SNR_file7='SNR_Noise0.0007.csv'
+	#SNR_file8='SNR_Noise0.0008.csv'
+	#SNR_file9='SNR_Noise0.0009.csv'
+	MeanEnergy_file='MeanEnergy7500.csv'
+	
+
+	SNR1=np.loadtxt(SNR_file1,delimiter=',')	
+	#SNR2=np.loadtxt(SNR_file2,delimiter=',')	
+	#SNR3=np.loadtxt(SNR_file3,delimiter=',')	
+	#SNR4=np.loadtxt(SNR_file4,delimiter=',')	
+	#SNR5=np.loadtxt(SNR_file5,delimiter=',')	
+	#SNR6=np.loadtxt(SNR_file6,delimiter=',')	
+	#SNR7=np.loadtxt(SNR_file7,delimiter=',')	
+	#SNR8=np.loadtxt(SNR_file8,delimiter=',')	
+	#SNR9=np.loadtxt(SNR_file9,delimiter=',')	
+	MeanEnergy=np.loadtxt(MeanEnergy_file,delimiter=',')	
+
+	fig=plt.figure()
+	
+	
+	plt.scatter(MeanEnergy,SNR1,s=10,label='0.0000537')
+	#plt.scatter(MeanEnergy,SNR2,s=10,label='0.0002')
+	#plt.scatter(MeanEnergy,SNR3,s=10,label='0.0003')
+	#plt.scatter(MeanEnergy,SNR4,s=10,label='0.0004')
+	#plt.scatter(MeanEnergy,SNR5,s=10,label='0.0005')
+	#plt.scatter(MeanEnergy,SNR6,s=10,label='0.0006')
+	#plt.scatter(MeanEnergy,SNR7,s=10,label='0.0007')
+	#plt.scatter(MeanEnergy,SNR8,s=10,label='0.0008')
+	#plt.scatter(MeanEnergy,SNR9,s=10,label='0.0009')
+	
+	plt.xlabel('MeanEnergy')
+	plt.ylabel('SNR')
+	
+	plt.title('SNR-MeanEnergy')
+	plt.legend(loc='upper left')
+	filename='SNR-MeanEnergy_Noise.png'
+	plt.savefig(filename)
+	plt.close()
+
+Check_SNR_MeanEnergy()
