958ba3031bb10f09356822018a2a4f73318fb1f9
Takayuki Higuchi <higuchitakayuki.tokushima@gmail.com>
2020-11-13 00:18:20 +0900

    CT_ED

--- CTtoDensity.py
+++ CTtoDensity.py
@@ -9,9 +9,15 @@ def definition_density()
 	
 	return density,density
 
+def actual_CTvalues()
+	B3F =[-1006.4,-726.6,-560.5,-84.2,-40.3,-1.6,-2.5,21,63.9,203.8,219.1,598.3,1058.6,1581.3]
+	QQ = [-1006.7,-704.3,-542.7,-94.4,-51.5,-13.3,-12.8,12.2,58.7,203,210.9,491.7,895.6,1338.3]
+	New =[-1006.7,-706.9,-533,-94.4,-44.5,-3.5,-3.7,18.4,68.9,218.7,230.1,428,795.1,1246.6]
+	
+	return B3F,QQ,New
+
 def CT_ED_SB3_highest(data):
 	CT_values=np.array(data)
-
 	CT_max=0
 	fig = plt.figure()
 	for i in range(0,570):
@@ -25,19 +31,13 @@ def CT_ED_SB3_highest(data):
 	plt.close()
 
 
+#実測のカーブと最も近いデータセットのカーブの番号を出力
+def CT_ED_nearest(data):
+	CT_values=np.array(data)
 
-def CTtoDensity_NearestCurve(csv_name):
-	f1=np.loadtxt(csv_name,delimiter=',')
-
-	CT_values=np.array(f1)
-
-	B3F =[-1006.4,-726.6,-560.5,-84.2,-40.3,-1.6,-2.5,21,63.9,203.8,219.1,598.3,1058.6,1581.3]
-	QQ = [-1006.7,-704.3,-542.7,-94.4,-51.5,-13.3,-12.8,12.2,58.7,203,210.9,491.7,895.6,1338.3]
-	New =[-1006.7,-706.9,-533,-94.4,-44.5,-3.5,-3.7,18.4,68.9,218.7,230.1,428,795.1,1246.6]
-
-	B3F_nearest=1000000
-	QQ_nearest=1000000
-	New_nearest=1000000
+	B3F_nearest=1000000000
+	QQ_nearest=1000000000
+	New_nearest=1000000000
 
 	dif_B3F=np.sum(np.square(CT_values-B3F),axis=1)
 	dif_QQ =np.sum(np.square(CT_values-QQ) ,axis=1)
@@ -54,10 +54,6 @@ def CTtoDensity_actual_fig():
 	ax.spines['top'].set_visible(False)
 	"""
 
-	B3F =[-1006.4,-726.6,-560.5,-84.2,-40.3,-1.6,-0.8,-2.5,21,63.9,203.8,219.1,598.3,1058.6,1581.3]
-	QQ = [-1006.7,-704.3,-542.7,-94.4,-51.5,-13.3,-15.8,-12.8,12.2,58.7,203,210.9,491.7,895.6,1338.3]
-	New =[-1006.7,-706.9,-533,-94.4,-44.5,-3.5,-2.1,-3.7,18.4,68.9,218.7,230.1,428,795.1,1246.6]
-	
 	
 	#fig=plt.figure()
 	
@@ -79,7 +75,6 @@ def CTtoDensity_fig(csv_name):
 	ax.spines['right'].set_visible(False)
 	ax.spines['top'].set_visible(False)
 	
-	density=[0.002,0.28,0.40,0.942,0.977,1.0,1.018,1.053,1.097,1.143,1.154,1.335,1.56,1.825]
 	f1=np.loadtxt(csv_name,delimiter=',')
 	CT_number=np.array(f1)
 	
@@ -90,7 +85,7 @@ def CTtoDensity_fig(csv_name):
 		else:
 			plt.scatter(CT_number[i,:],density,marker="^",s=10,alpha=0.5,color='Cyan')
 	
-def CTtoDensity_Interpolation_fig(csv_name):
+def interpolation_fig(data):
 	"""
 	ax = fig.add_subplot(111)
 	
@@ -98,22 +93,13 @@ def CTtoDensity_Interpolation_fig(csv_name):
 	ax.spines['right'].set_visible(False)
 	ax.spines['top'].set_visible(False)
 	"""
-	density=[0.002,0.28,0.40,0.942,0.977,1.0,1.018,1.053,1.097,1.143,1.154,1.335,1.56,1.825]
-	f1=np.loadtxt(csv_name,delimiter=',')
-	CT_number=np.array(f1)
+	CT_number=np.array(data)
 	plt.scatter(CT_number[0,:],density,marker="^",s=10,color='blue',label='B3F')
 	plt.scatter(CT_number[1,:],density,marker="^",s=10,color='red',label='QQ')
 	plt.scatter(CT_number[2,:],density,marker="^",s=10,color='green',label='New')
 	
 
-def Interpolation():
-	density_actual=[0,0.3,0.48,0.949,0.984,1,1.018,1.051,1.09,1.138,1.146,1.333,1.56,1.822]
-
-	B3F =[-1006.4,-726.6,-560.5,-84.2,-40.3,-1.6,-2.5,21,63.9,203.8,219.1,598.3,1058.6,1581.3]
-	QQ = [-1006.7,-704.3,-542.7,-94.4,-51.5,-13.3,-12.8,12.2,58.7,203,210.9,491.7,895.6,1338.3]
-	New =[-1006.7,-706.9,-533,-94.4,-44.5,-3.5,-3.7,18.4,68.9,218.7,230.1,428,795.1,1246.6]
-	
-	density=[0.002,0.28,0.40,0.942,0.977,1.0,1.018,1.053,1.097,1.143,1.154,1.335,1.56,1.825]
+def interpolation():
 	delta_x_B3F=np.empty(len(density)-1)
 	delta_x_QQ=np.empty(len(density)-1)
 	delta_x_New=np.empty(len(density)-1)
